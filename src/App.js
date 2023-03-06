0173cd74d0258df218ac10a44616b8aae3be74ec
Aryan Tayal <bookworms@ariansh.com>
2023-03-06 09:03:43 +0530

    add keyboard navigation

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ import Page from "./Page";
 
 const App = () => {
   const [pageDisplayed, setPageDisplayed] = useState(0);
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 38 && pageDisplayed > 0) {
+      setPageDisplayed(pageDisplayed - 1);
+    } else if (e.keyCode === 40 && pageDisplayed < characters.length - 1) {
+      setPageDisplayed(pageDisplayed + 1);
+    }
+  };
   return (
-    <div className="App">
+    <div className="App" onKeyDown={(e) => handleKeyDown(e)} tabIndex="0">
       <Nav currentPage={pageDisplayed} changePage={setPageDisplayed} />
       {characters.map((c, i) => (
         <Page {...c} key={i} index={i} hide={pageDisplayed > i} />
