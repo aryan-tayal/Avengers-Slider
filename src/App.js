6f082a20410c91dc3c396277a189966a0b2a4bfd
Aryan Tayal <bookworms@ariansh.com>
2023-03-07 05:50:33 +0530

    fix keyboard navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import characters from "./helper";
 
@@ -7,15 +7,22 @@ import Page from "./Page";
 
 const App = () => {
   const [pageDisplayed, setPageDisplayed] = useState(0);
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 38 && pageDisplayed > 0) {
+
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.focus();
+  }, []);
+
+  const handleKeyDown = ({ keyCode }) => {
+    if (keyCode === 38 && pageDisplayed > 0) {
       setPageDisplayed(pageDisplayed - 1);
-    } else if (e.keyCode === 40 && pageDisplayed < characters.length - 1) {
+    } else if (keyCode === 40 && pageDisplayed < characters.length - 1) {
       setPageDisplayed(pageDisplayed + 1);
     }
   };
   return (
-    <div className="App" onKeyDown={(e) => handleKeyDown(e)} tabIndex="0">
+    <div className="App" onKeyDown={handleKeyDown} tabIndex="0" ref={divRef}>
       <Nav currentPage={pageDisplayed} changePage={setPageDisplayed} />
       {characters.map((c, i) => (
         <Page {...c} key={i} index={i} hide={pageDisplayed > i} />
