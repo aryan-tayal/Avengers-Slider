c95dcd8bdb5e0ed90b3c892f56c35501103d463c
Aryan Tayal <bookworms@ariansh.com>
2023-03-07 07:03:36 +0530

    add aria labels to navbuttons

--- src/NavButton.js
+++ src/NavButton.js
@@ -2,6 +2,8 @@ const NavButton = ({ index, handleClick, active }) => (
   <button
     className={`NavButton ${active && "active"}`}
     onClick={handleClick}
+    aria-label="Slide Toggle Button"
+    aria-pressed={active}
   ></button>
 );
 
